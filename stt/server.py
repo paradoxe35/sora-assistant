00a7f926ca6ba3ab79c12e8ec2938dacccfd3e4b
Paradoxe Ngwasi <paradoxengwasi@gmail.com>
2022-01-22 20:44:39 +0200

    runnable sr- server argument

--- stt/server.py
+++ stt/server.py
@@ -1,7 +1,9 @@
 from local.asr_server_en import start as start_en_server
 from local.asr_server_fr import start as start_fr_server
+from third_party.speech_recognition import start as start_sr
 import sys
 import logging
+from env import fr_server_port, en_server_port
 
 
 if __name__ == '__main__':
@@ -11,6 +13,10 @@ if __name__ == '__main__':
             start_en_server()
         elif executor == 'local-fr':
             start_fr_server()
+        elif executor == 'sr-fr':
+            start_sr('fr-FR', fr_server_port())
+        elif executor == 'sr-en':
+            start_sr('en-US', en_server_port())
         else:
             logging.warning('unknown server argument')
     else:
