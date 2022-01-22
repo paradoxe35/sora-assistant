eed10d080edda9b29d7857c9f9915c55926a7b10
Paradoxe Ngwasi <paradoxengwasi@gmail.com>
2022-01-23 00:01:44 +0200

    env server port variable by server type

--- stt/env.py
+++ stt/env.py
@@ -2,11 +2,11 @@ import os
 
 
 def fr_server_port():
-    return int(os.environ.get('STT_SERVER_PORT', 2782))
+    return int(os.environ.get('STT_FR_SERVER_PORT', 2782))
 
 
 def en_server_port():
-    return int(os.environ.get('STT_SERVER_PORT', 2781))
+    return int(os.environ.get('STT_EN_SERVER_PORT', 2781))
 
 
 def server_interface():
