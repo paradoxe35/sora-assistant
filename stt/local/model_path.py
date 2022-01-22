1312210abb66e4c8e7402a6ea7c3da400630f74b
Paradoxe Ngwasi <paradoxengwasi@gmail.com>
2022-01-23 00:02:40 +0200

    env model path varible by model type

--- stt/local/model_path.py
+++ stt/local/model_path.py
@@ -2,8 +2,8 @@ import os
 
 
 def fr_model_path() -> str:
-    return os.environ.get('VOSK_MODEL_PATH', 'stt/local/model/fr')
+    return os.environ.get('VOSK_FR_MODEL_PATH', 'stt/local/model/fr')
 
 
 def en_model_path() -> str:
-    return os.environ.get('VOSK_MODEL_PATH', 'stt/local/model/en')
+    return os.environ.get('VOSK_EN_MODEL_PATH', 'stt/local/model/en')
