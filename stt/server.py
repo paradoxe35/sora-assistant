50dbae2b5d8e6ee8a6d3ef72a78af7266e43f598
Paradoxe Ngwasi <paradoxengwasi@gmail.com>
2022-01-23 09:16:09 +0200

    load dotenv

--- stt/server.py
+++ stt/server.py
@@ -1,10 +1,14 @@
+
 from local.asr_server_en import start as start_en_server
 from local.asr_server_fr import start as start_fr_server
 from third_party.speech_recognition import start as start_sr
 import sys
 import logging
-from env import fr_server_port, en_server_port
+from dotenv import load_dotenv
 
+load_dotenv()
+
+from env import fr_server_port, en_server_port  # noqa
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
